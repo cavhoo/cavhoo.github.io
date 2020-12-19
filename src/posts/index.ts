d940326d9e390a520004ff5e5dc856a65954243d
Hendrik Mueller-Roehr <darksolj@live.com>
2020-12-19 12:27:07 +0100

    Blog: December post

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,9 +1,11 @@
 import FirstPost from "./firstPost.json"
 import NineteenthOctober from "./20201019.json"
+import NineteenthDecember from "./20201219.json"
 
 const Posts = [
   FirstPost,
-  NineteenthOctober
+  NineteenthOctober,
+  NineteenthDecember
 ]
 
 export default Posts
