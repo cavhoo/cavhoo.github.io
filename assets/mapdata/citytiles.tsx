f535d014f3356c94a445e4d0559c151fd01b4bc2
cavhoo <darksolj@live.com>
2026-05-12 22:05:55 +0200

    Updates

---
+++ assets/mapdata/citytiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="citytiles" tilewidth="32" tileheight="32" tilecount="6077" columns="59">
+ <image source="../textures/citytiles.png" width="1888" height="3296"/>
+</tileset>
